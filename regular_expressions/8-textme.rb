2789872b69c19e0bd6bc46c3f29c0a05c5a05b25
danjor667 <jordannguepi@gmail.com>
2023-09-10 14:19:33 +0200

    retry task 8

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -4,4 +4,4 @@ from = /(?<=from:).[[a-z|+?0-9]]*/i
 to = /(?<=to:).[[a-z|?0-9]]*/i
 flags = /(?<=flags:).[-?\d+[:]-?\d]*/
 
-puts "#{ARGV[0].scan(from)[0]}, #{ARGV[0].scan(to)[0]}, #{ARGV[0].scan(flags)[0]}"
+puts "#{ARGV[0].scan(from)[0]},#{ARGV[0].scan(to)[0]},#{ARGV[0].scan(flags)[0]}"
